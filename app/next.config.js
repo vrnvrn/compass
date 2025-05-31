09ee85dd3e828a07ae979d701bed316c25244bc2
vrnvrn <vrneth@proton.me>
2025-06-01 01:44:36 +0200

    chore: downgrade React and related types to version 18.x in package.json and package-lock.json for compatibility

---
+++ app/next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    appDir: true,
+    serverActions: {
+      enabled: true
+    },
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
+
+module.exports = nextConfig 
